0e8fd5c59e9604691b5a00507e6a0e8f82e361a9
Georg Lukas <georg@op-co.de>
2012-03-04 14:16:05 +0100

    namespace change: org.yaxim.androidclient -> .bruno
    sed -i 's/org.yaxim.androidclient/org.yaxim.bruno/g' `grep org.yaxim.androidclient -rl res src` AndroidManifest.xml
    mkdir src/org/yaxim/bruno/
    git mv src/org/yaxim/androidclient/* src/org/yaxim/bruno/
    sed -i 's/yaxim/Bruno/g' res/values*/*
    sed -i 's/"yaxim"/"Bruno"/g' src/org/yaxim/bruno/data/YaximConfiguration.java
    git add res src AndroidManifest.xml

--- src/org/yaxim/androidclient/util/AdapterConstants.java
+++
@@ -1,8 +0,0 @@
-package org.yaxim.androidclient.util;
-
-public class AdapterConstants {
-	public static final String[] GROUP_NAME = {"groupName"};
-	public static final String[] CHILD_DATA_KEYS = {"screenName","statusMode"};
-	public static final String 	 CONTACT_ID  = "contactId";
-	public static final String EMPTY_GROUP = "General";
-}
